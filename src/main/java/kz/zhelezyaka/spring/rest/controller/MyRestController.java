9e994c89b82c4b6590d84c23dc2751fb166bf7d1
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2022-05-04 21:14:53 +0600

    Add new employee

--- src/main/java/kz/zhelezyaka/spring/rest/controller/MyRestController.java
+++ src/main/java/kz/zhelezyaka/spring/rest/controller/MyRestController.java
@@ -33,4 +33,10 @@ public class MyRestController {
         }
         return employee;
     }
+
+    @PostMapping("/employees")
+    public Employee addNewEmployee(@RequestBody Employee employee) {
+        employeeService.saveEmployee(employee);
+        return employee;
+    }
 }
