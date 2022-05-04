19c015e149eaea5f19bdedf30a8636a53e11a006
Vladimir Glinskih <vladimir.v.glinskikh@gmail.com>
2022-05-04 22:02:35 +0600

    Changing an existing employee

--- src/main/java/kz/zhelezyaka/spring/rest/controller/MyRestController.java
+++ src/main/java/kz/zhelezyaka/spring/rest/controller/MyRestController.java
@@ -39,4 +39,10 @@ public class MyRestController {
         employeeService.saveEmployee(employee);
         return employee;
     }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee) {
+        employeeService.saveEmployee(employee);
+        return employee;
+    }
 }
